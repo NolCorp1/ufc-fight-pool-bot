e104c4d49b9ff7c65cb2d7a4779c0f061d683898
NolCorp <imnolann@gmail.com>
2024-11-16 09:11:12 -0500

    update URL

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ app.listen(port, () => {
 });
 
 // URL of the Wikipedia page with the table you want to scrape
-const url = 'https://en.wikipedia.org/wiki/UFC_308';
+const url = 'https://en.wikipedia.org/wiki/UFC_309';
 
 // Function to scrape and process the UFC fight card data
 async function scrapeUFCCard() {
